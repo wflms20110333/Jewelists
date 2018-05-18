80ec4b80ff35575ae882c86613fffd4f2b6a63db
Elizabeth Zou <elizabeth.zou.0@gmail.com>
2018-05-18 14:09:28 -0500

    comments pt2

--- src/data/Ability.java
+++ src/data/Ability.java
@@ -12,23 +12,22 @@ import helpers.Artist;
  */
 public enum Ability
 {
-	// buff
 	/**
-	 * Speeds up a player's movement.
+	 * Speeds up a player's movement. This is a buff.
 	 */
 	SPEED("ability_speed", true),
 	
 	/**
-	 * Boosts the damages of a player's attacks.
+	 * Boosts the damages of a player's attacks. This is a buff.
 	 */
 	DMG_BOOST("ability_dmg_boost", true),
 	
 	/**
-	 * Gives a player the ability to collect the jewels in adjacent cells.
+	 * Gives a player the ability to collect the jewels in adjacent cells. This
+	 * is a buff.
 	 */
 	MAGNET("ability_magnet", true, 3),
 	
-	// activated instantly
 	/**
 	 * Teleports a player's sprite in its current direction.
 	 */
@@ -45,33 +44,85 @@ public enum Ability
 	 */
 	SLOW("ability_slow", true, 3);
 	
+	/**
+	 * All the possible abilities.
+	 */
 	private static final Ability[] abilities = values();
 	
+	/**
+	 * The length of the durations of buffs (in seconds).
+	 */
 	public static final long BUFF_DURATION = 10;
+	
+	/**
+	 * The length of the cool downs of abilities (in seconds).
+	 */
 	public static final long BUFF_COOLDOWN = 10;
 	
+	/**
+	 * The duration (in seconds) of the Ability, -1 if it is not a buff.
+	 */
 	private final float duration;
+	
+	/**
+	 * The length of the cool down (in seconds) of the Ability.
+	 */
 	private final float cooldown;
+	
+	/**
+	 * The value of the Ability.
+	 */
 	private final int value;
+	
+	/**
+	 * The name of the Ability.
+	 */
 	private final String name;
 	
+	/**
+	 * The texture of the Ability.
+	 */
 	private final Texture texture;
 	
+	/**
+	 * Constructs an Ability that is not a buff.
+	 * 
+	 * @param name the name of the ability
+	 */
 	private Ability(String name)
 	{
 		this(name, false);
 	}
 	
+	/**
+	 * Constructs an Ability.
+	 * 
+	 * @param name the name of the ability
+	 * @param buff whether the ability is a buff
+	 */
 	private Ability(String name, boolean buff)
 	{
 		this(name, buff, -1);
 	}
 	
+	/**
+	 * Constructs an Ability with a value.
+	 * 
+	 * @param name the name of the ability
+	 * @param value the value of the ability
+	 */
 	private Ability(String name, int value)
 	{
 		this(name, false, value);
 	}
 	
+	/**
+	 * Constructs an Ability.
+	 * 
+	 * @param name the name of the ability
+	 * @param buff whether the ability is a buff
+	 * @param value the value of the ability
+	 */
 	private Ability(String name, boolean buff, int value)
 	{
 		if (buff)
@@ -84,31 +135,61 @@ public enum Ability
 		this.texture = Artist.quickLoad(name);
 	}
 	
+	/**
+	 * Returns the duration (in seconds) of the ability.
+	 * 
+	 * @return the duration (in seconds) of the ability
+	 */
 	public float getDuration()
 	{
 		return duration;
 	}
 	
+	/**
+	 * Returns the length of the cool down (in seconds) of the ability.
+	 * 
+	 * @return the length of the cool down (in seconds) of the ability
+	 */
 	public float getCooldown()
 	{
 		return cooldown;
 	}
 	
+	/**
+	 * Returns the value of the ability.
+	 * 
+	 * @return the value of the ability
+	 */
 	public long getValue()
 	{
 		return value;
 	}
 	
+	/**
+	 * Returns the texture of the ability.
+	 * 
+	 * @return the texture of the ability
+	 */
 	public Texture getTexture()
 	{
 		return texture;
 	}
 	
+	/**
+	 * Returns a random ability.
+	 * 
+	 * @return a random ability
+	 */
 	public static Ability random()
 	{
 		return abilities[(int) (Math.random() * abilities.length)];
 	}
 	
+	/**
+	 * Returns the name of the ability.
+	 * 
+	 * @return the name of the ability
+	 */
 	@Override
 	public String toString()
 	{
